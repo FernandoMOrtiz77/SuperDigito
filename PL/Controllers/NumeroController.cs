1cd7799f67fbceb8a9d0a6eaaec03040c3be8d1c
fhdhg <asdxcghjkhj>
2026-01-08 16:58:13 -0600

    Cambios

--- PL/Controllers/NumeroController.cs
+++ PL/Controllers/NumeroController.cs
@@ -8,34 +8,7 @@ namespace PL.Controllers
 {
     public class NumeroController : Controller
     {
-        // GET: Numero
-        [HttpGet]
-        public ActionResult SuperDigito()
-        {
-            ML.Result result = BL.Numero.GetAll();
-            return View(result);
-        }
-
-        [HttpPost]
-        public ActionResult SuperDigito(string numero)
-        {
-
-            int resultado = BL.Numero.CalcularSuperDigito(numero);
-
-            BL.Numero.Insertar(numero, resultado);
-
-            ML.Result result = BL.Numero.GetAll();
-
-            ViewBag.SuperDigito = resultado;
-
-            return View(result);
-        }
-
-        public ActionResult Delete()
-        {
-            BL.Numero.Delete();
-            return RedirectToAction("SuperDigito");
-        }
+        
         //Con Linq
         public ActionResult CRUDLinq()
         {
