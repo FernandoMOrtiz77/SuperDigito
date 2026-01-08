9473d25a00f7d6f8c01d7c124fba7129148d7c0a
FernandoMOrtiz77 <fernandomadariaga1@outlook.com>
2026-01-08 17:01:14 -0600

    Revert "Revert "Union LINQ""

--- PL/Controllers/NumeroController.cs
+++ PL/Controllers/NumeroController.cs
@@ -36,5 +36,57 @@ namespace PL.Controllers
             BL.Numero.Delete();
             return RedirectToAction("SuperDigito");
         }
+        //Con Linq
+        public ActionResult CRUDLinq()
+        {
+            ML.Result result = BL.Numero.GetAllLINQ();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(string numero)
+        {
+            int resultado = BL.Numero.CalcularSuperDigito(numero);
+
+            ML.Numerox numerox = new ML.Numerox
+            {
+                Numero = int.Parse(numero),
+                Resultado = resultado
+            };
+
+            BL.Numero.AddLINQ(numerox);
+
+            return RedirectToAction("CRUDLinq");
+        }
+        public ActionResult DeleteLINQ(int id)
+        {
+            BL.Numero.DeleteLINQ(id);
+            return RedirectToAction("CRUDLinq");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ML.Result result = BL.Numero.GetByIdLINQ(id);
+            if(result.Correct)
+            {
+                return View((ML.Numerox)result.Object);
+            }
+            return RedirectToAction("CRUDLinq");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ML.Numerox numerox)
+        {
+            numerox.Resultado = BL.Numero.CalcularSuperDigito(numerox.Numero.ToString());
+            BL.Numero.UpdateLINQ(numerox);
+            return RedirectToAction("CRUDLinq");
+        }
     }
 }
